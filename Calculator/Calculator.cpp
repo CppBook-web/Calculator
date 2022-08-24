029810d00a36e24ffbdbc3dad9329f3f3372735b
User <User@ANDROID>
2022-08-24 16:20:00 +0300

    Добавьте файлы проекта.

---
+++ Calculator/Calculator.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "rus");
+
+    double x = 0.0;
+    double y = 0.0;
+    double result = 0.0;
+    char oper = ' ';
+
+    cout << "Приложение калькулятор." << endl;
+    cout << "Введите выражение для вычисления. Формат: a + b, a - b, a * b, a / b."
+        << "\nВвод:";
+
+    while (true)
+    {
+        cin >> x >> oper >> y;
+        if (oper == '/' && y == 0)
+        {
+            cout << "Деление на ноль запрещено" << endl;
+            continue;
+        }
+        else
+        {
+            switch (oper)
+            {
+            case '+':
+                result = x + y;
+                break;
+            case '-':
+                result = x - y;
+                break;
+            case '*':
+                result = x * y;
+                break;
+            case '/':
+                result = x / y;
+                break;
+            default:
+                result = 0.0;
+            }
+        }
+        cout << "Результат: " << result << endl;
+    }
+
+    return 0;
+}
